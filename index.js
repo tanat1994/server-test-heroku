7ccfb51869cb7f2b94aa6fd619dd86d6d8f12207
tanat1994 <tanat1994@gmail.com>
2018-05-24 15:39:04 +0700

    mod. index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ hi: 'there',
+            bye: 'buddy'});
 });
 
 const PORT = process.env.PORT || 3000;
